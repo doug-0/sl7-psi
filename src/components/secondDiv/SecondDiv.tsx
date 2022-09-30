65c828cce822929b93c8aa4123ba3654c2a1281b
douglas oliveira <douglasgama0@gmail.com>
2022-09-30 19:28:54 -0300

    feat: adicionando div Dois

---
+++ src/components/secondDiv/SecondDiv.tsx
@@ -0,0 +1,13 @@
+import './secondDiv.css';
+
+export default function SecondDiv(): JSX.Element {
+  return (
+    <div className='divOne'>
+      <div className='divText'>
+        <p>
+          ...
+        </p>
+      </div>
+    </div>
+  );
+}
